d7e7fe0667cc147bf8c9f95617225a9296b95186
Nerwal2 <master4567890@gmail.com>
2021-10-06 11:28:26 +0000

    ale_piwko

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import Harnas from './component/Harnas';
+
+
+   const app = (
+       <>
+       <Harnas />
+       </>
+   )
+    ReactDOM.render(app, document.getElementById('root'));
