e9e9e0c3d1993dc143e8927552a88ed300ffeec5
Nerwal2 <master4567890@gmail.com>
2021-10-07 09:14:30 +0000

    update

--- src/component/Harnas.js
+++ src/component/Harnas.js
@@ -5,31 +5,20 @@ import ReactDOM from 'react-dom'
 class Harnas extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            Harnas: 0,
-            Eur: 0,
-        };
-    }
-
-    changeCurency(value,scale){
-        switch(scale){
-            case "Eur":
-                this.setState({Harnas: value * 1.84 , Eur: value});
-                return;
-            case "Harnaś":
-                this.setState({Eur: value * 0.56, Harnas: value});
-                return;
-        }
     }
 
     render(){
         return<>
-        <div id="piwo">
-        <p>Harnaś</p>
-        <input type="text" value={this.state.Harnas} onChange={(event) => this.changeCurency(event.target.value, "Harnaś") }/>
-        <p>Eur</p>
-        <input type="text" value={this.state.Eur} onChange={(event) => this.changeCurency(event.target.value, "Eur") }/>
-        </div>
+        <p>{this.props.beerName}</p>
+        <input 
+        type="text" 
+        value={this.props.beerValue} 
+        onChange={(event) => {
+            this.props.changeHarnas(
+                event.target.value, this.props.beerName
+                    )
+                }
+            }/>
         </>
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
 import Harnas from './component/Harnas';
 
 
-   const app = (
-       <>
-       <Harnas />
-       </>
-   )
-    ReactDOM.render(app, document.getElementById('root'));
+class ConverHarnas extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            Harnas: 0,
+            Eur: 0,
+        };
+        this.changeHarnas = this.changeHarnas.bind(this)
+    }
+
+    changeHarnas(value,scale){
+        switch(scale){
+            case "Eur":
+                this.setState({Harnas: value * 1.84 , Eur: value});
+                return;
+            case "Harnas":
+                this.setState({Eur: value * 0.56, Harnas: value});
+                return;
+        }
+    }
+
+    render(){
+        return (
+        <>
+       <Harnas
+           beerName = "Harnas"
+           beerValue = {this.state.Harnas}
+           changeHarnas = {this.changeHarnas}
+       />
+       <Harnas
+           beerName = "Eur"
+           beerValue = {this.state.Eur}
+           changeHarnas = {this.changeHarnas}
+       />
+        </>
+        );
+    }
+}
+
+ReactDOM.render(<ConverHarnas />, document.getElementById("root"))
